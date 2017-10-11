17e374f13546134142490b46556dbc53b94eeab7
Robophil <oghenerobo@gmail.com>
2017-10-11 17:19:38 +0100

    feat(packgage.json): add package.json for project init

--- bin/cmd/new.js
+++ bin/cmd/new.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra')
 const path = require('path')
+const replace = require('replace-in-file')
 const cwd = process.cwd()
 
 const postfixName = '-service'
@@ -24,8 +25,19 @@ module.exports = (name, options) => {
     const src = path.resolve(__dirname, '..', '..', 'files/action')
     const dest = `${cwd}/${name}`
 
-    return fs.copy(src, dest)
+    const configSrc = path.resolve(__dirname, '..', '..', 'files/config')
+    const configDest = `${cwd}/${name}/config`
+
+    return Promise.all([fs.copy(src, dest), fs.copy(configSrc, configDest)])
   }).then(() => {
-    console.log('made new projject')
+    console.log(`\n\tNew Project has been created at '${name}'`)
+    const dest = `${cwd}/${name}/package.json`
+    return replace({
+      files: `${dest}`,
+      from: /microservice-lite/g,
+      to: `${name}`
+    }).then(() => {
+      console.log(`\tcd into ${name} and run 'npm install'\n\trun 'mslite help' for assistance and \n\trun 'npm start' to see your project up and running\n`)
+    })
   })
 }
