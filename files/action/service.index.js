4a6a6e39e52afa728ec0e9c2f05899ce0313c2fe
Robophil <oghenerobo@gmail.com>
2017-10-12 17:58:50 +0100

    fix(service): fixed recursive search of serices.
    Global name for requester and publishers now CamelCase

--- files/action/service.index.js
+++ files/action/service.index.js
@@ -3,7 +3,11 @@ const path = require('path')
 // require publishers and make them global
 const publishers = require('require-all')({
   'dirname': path.resolve(__dirname) + '/src/app',
-  'filter': /(.+publisher)\.js$/
+  'recursive': false,
+  'filter': function (fileName) {
+    let parts = fileName.split('.publisher')
+    if (parts[1] === '.js') return `${parts[0].charAt(0).toUpperCase() + parts[0].slice(1)}Publisher`
+  }
 })
 Object.keys(publishers).forEach(publisher => {
   global[publisher] = publishers[publisher]
@@ -12,8 +16,13 @@ Object.keys(publishers).forEach(publisher => {
 // require requesters and make them global
 const requesters = require('require-all')({
   'dirname': path.resolve(__dirname) + '/src/app',
-  'filter': /(.+requester)\.js$/
+  'recursive': false,
+  'filter': function (fileName) {
+    let parts = fileName.split('.requester')
+    if (parts[1] === '.js') return `${parts[0].charAt(0).toUpperCase() + parts[0].slice(1)}Requester`
+  }
 })
+
 Object.keys(requesters).forEach(requester => {
   global[requester] = requesters[requester]
 })
@@ -21,10 +30,12 @@ Object.keys(requesters).forEach(requester => {
 // require all responders
 require('require-all')({
   'dirname': path.resolve(__dirname) + '/src/app',
-  'filter': /(.+responder)\.js$/
+  'filter': /(.+responder)\.js$/,
+  'recursive': false
 })
 // require all subscribers
 require('require-all')({
   'dirname': path.resolve(__dirname) + '/src/app',
-  'filter': /(.+subscriber)\.js$/
+  'filter': /(.+subscriber)\.js$/,
+  'recursive': false
 })
